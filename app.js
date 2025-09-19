1d2af2abc64ad0d22c49355967993c07adddc02f
darthstar22 <deskremotetop@gmail.com>
2025-09-19 12:14:43 -0400

    f

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 function getHello() {
     return {
-        hello: "ciao",
+        hello: "\u00A0", // placeholder to start (non-breaking space)
         greetings: [
             "Hola",         // Spanish
             "你好",          // Mandarin Chinese
             "Hello",        // English
             "हेलो",         // Hindi
-            "مرحباً",       // Arabic
             "Bonjour",      // French
             "Привет",       // Russian
-            "Haloa",         // Hawaiian
+            "Haloa",        // Hawaiian
             "こんにちは",    // Japanese
             "Olá",          // Portuguese
             "안녕하세요",    // Korean
@@ -22,16 +21,54 @@ function getHello() {
             "Hej",          // Swedish / Danish
             "Shalom",       // Hebrew
         ],
+
         index: 0,
-        tick() {
-            this.hello = this.greetings[this.index].toLowerCase()
-            this.index = (this.index + 1) % this.greetings.length
+        typingInterval: null,
+        mode: "typing", // typing or deleting
+
+        typeText(text) {
+            let word = text.toLowerCase()     // convert to lowercase here
+            let i = 0
+            this.hello = "\u00A0" // reset but keep placeholder
+            this.mode = "typing"
+
+            this.typingInterval = setInterval(() => {
+                if (this.mode === "typing") {
+                    // remove placeholder before adding first char
+                    if (this.hello === "\u00A0") this.hello = ""
+                    this.hello += word[i]
+                    i++
+                    if (i >= word.length) {
+                        this.mode = "waiting"
+                        clearInterval(this.typingInterval)
+                        setTimeout(() => this.deleteText(), 1000) // pause before delete
+                    }
+                }
+            }, 150) // typing speed
         },
+
+        deleteText() {
+            this.mode = "deleting"
+            this.typingInterval = setInterval(() => {
+                // keep at least placeholder
+                if (this.hello.length > 1 || this.hello !== "\u00A0") {
+                    this.hello = this.hello.slice(0, -1) || "\u00A0"
+                }
+
+                if (this.hello === "\u00A0") {
+                    clearInterval(this.typingInterval)
+                    this.index = (this.index + 1) % this.greetings.length
+                    setTimeout(() => this.typeText(this.greetings[this.index]), 300) // pause before next word
+                }
+            }, 100) // deleting speed
+        },
+
         init() {
-            this.interval = setInterval(() => this.tick(), 1000)
+            this.typeText(this.greetings[this.index])
         },
+
         destroy() {
-            clearInterval(this.interval)
+            clearInterval(this.typingInterval)
         }
     }
 }
